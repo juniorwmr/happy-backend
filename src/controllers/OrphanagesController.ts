938867d24cb5a0e36fa926263939a223cfd081f9
Washington Muniz <juniorripardo@gmail.com>
2020-10-13 11:51:50 -0300

    Orphanage's controllers with index and create

---
+++ src/controllers/OrphanagesController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import { Orphanage } from '../models/Orphanage';
+
+export default {
+  async index(_: any, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanages = await orphanagesRepository.find();
+
+    return response.status(200).json(orphanages);
+  },
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    });
+    await orphanagesRepository.save(orphanage);
+
+    return response.status(201).json(orphanage);
+  },
+};
