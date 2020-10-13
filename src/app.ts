d84a96c0cd8eb8322a46fcf2b110eb46797edd3b
Washington Muniz <juniorripardo@gmail.com>
2020-10-13 13:41:31 -0300

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,8 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
