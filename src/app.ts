0b705c29f5ee1eaf2e9514635a1375b760c56422
Washington Muniz <juniorripardo@gmail.com>
2020-10-13 13:15:13 -0300

    adding dotenv to hide host

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import path from 'path';
 import express from 'express';
 import cors from 'express';
--- src/views/images_view.ts
+++ src/views/images_view.ts
@@ -3,7 +3,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://localhost:3333/uploads/${image.path}`,
+      url: `${process.env.HOST}/uploads/${image.path}`,
     };
   },
   renderMany(images: Image[]) {
