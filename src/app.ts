c1a9b8ae9a3a5929548e29fa370ed816a08b970b
Washington Muniz <juniorripardo@gmail.com>
2020-10-13 13:39:40 -0300

    Validation and exceptions with Yup

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ require('dotenv').config();
 
 import path from 'path';
 import express from 'express';
+import 'express-async-errors';
 import cors from 'express';
 
 import './database/connection';
+import { errorHandler } from './errors/handler';
 
 import { routes } from './routes';
 
@@ -14,5 +16,8 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+// Handles Errors
+app.use(errorHandler);
 
 export { app };
