b076ea67350534940b77addacaf6a2683cccbeef
Washington Muniz <juniorripardo@gmail.com>
2020-10-13 11:53:15 -0300

    code refactoring

--- src/routes.ts
+++ src/routes.ts
@@ -1,33 +1,10 @@
-import { Orphanage } from './models/Orphanage';
 import { Router } from 'express';
-import { getRepository } from 'typeorm';
+
+import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 
-routes.post('/orphanages', async (request, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  });
-  await orphanagesRepository.save(orphanage);
-
-  return response.status(201).json(orphanage);
-});
+routes.get('/orphanages', OrphanagesController.index);
+routes.post('/orphanages', OrphanagesController.create);
 
 export { routes };
