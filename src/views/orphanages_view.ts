aadb645f9f652836069fcd28e666569fd10b9c12
Washington Muniz <juniorripardo@gmail.com>
2020-10-16 13:11:14 -0300

    ajusting tuple 'about' view

--- src/views/orphanages_view.ts
+++ src/views/orphanages_view.ts
@@ -8,7 +8,7 @@ export default {
       name: orphanage.name,
       latitude: orphanage.latitude,
       longitude: orphanage.longitude,
-      about: orphanage.id,
+      about: orphanage.about,
       instructions: orphanage.instructions,
       opening_hours: orphanage.opening_hours,
       open_on_weekends: orphanage.open_on_weekends,
