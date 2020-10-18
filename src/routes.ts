9d6b13c857022e8ca7bf7ad55220cae0d9e2e477
Washington Muniz <juniorripardo@gmail.com>
2020-10-17 22:23:25 -0500

    adding a middlware to private routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,15 +5,19 @@ import uploadConfig from './config/upload';
 
 import OrphanagesController from './controllers/OrphanagesController';
 import UsersController from './controllers/UsersController';
+import { AuthenticateMiddleware } from './middlewares/auth';
 
 const routes = Router();
 const upload = multer(uploadConfig);
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 
 routes.post('/users', UsersController.create);
+
 routes.post('/auth', UsersController.auth);
+
+routes.use(AuthenticateMiddleware);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 
 export { routes };
