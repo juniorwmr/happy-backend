b38c608ca7f4559a0e48fe53350242037bc4008c
Washington Muniz <juniorripardo@gmail.com>
2021-04-24 22:37:46 -0500

    fixing path issue

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,15 +1,13 @@
-const config = {
+export default {
   type: 'postgres',
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  migrations: [process.env.TYPEORM_MIGRATIONS],
-  entities: [process.env.TYPEORM_ENTITIES],
+  migrations: ['./src/database/migrations/**.ts'],
+  entities: ['./src/models/**.ts'],
   cli: {
-    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+    migrationsDir: './src/database/migrations',
   },
 };
-
-module.exports = config;
--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,6 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Handles Errors
 app.use(errorHandler);
